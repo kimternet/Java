9467d00a3f1987b6a32276b6fce6bbfe58b76e4c
Hyungwoon_Kim <126911740+kimternet@users.noreply.github.com>
2024-02-29 15:20:42 +0900

    #1 DataType~TypeCasting

---
+++ chap_01/_Quiz1_.java
@@ -0,0 +1,19 @@
+package chap_01;
+
+public class _Quiz1_ {
+    public static void main(String[] args) {
+
+        int BusNo_1 = 1234;
+        String BusNo_2 = "상암08";
+        int Min = 3;
+        int Min2 = 5;
+
+        float Way = 1.5f;
+        float Way2 = 0.8f;
+
+
+        System.out.println(BusNo_1+"번 버스가 "+ Min +"분 후 도착 예정입니다. 도착까지 "+ Way +"km 남았습니다.");
+        System.out.println(BusNo_2+"번 버스가 "+ Min2 +"분 후 도착 예정입니다. 도착까지 "+ Way2 +"km 남았습니다.");
+
+    }
+}
